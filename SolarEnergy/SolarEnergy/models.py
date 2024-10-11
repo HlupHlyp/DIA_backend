28131d7d245cc39d22c80eba7472c3f07578acc0
HlupHlyp <Dru0998@yandex.ru>
2024-10-11 18:36:53 +0000

    ORM models are corrected. Deleting of plant is working. Mechanisms are rebased from login to id

--- SolarEnergy/SolarEnergy/models.py
+++ SolarEnergy/SolarEnergy/models.py
@@ -1,35 +1,50 @@
 from django.db import models
 import uuid
-
+from datetime import datetime
 class item_model(models.Model):
+    item_statuses = {
+        'active': 'active',
+        'deleted': 'deleted',
+    }
+    item_types = {
+        'solar_panel': 'solar_panel',
+        'battery': 'battery',
+    }
     item_id = models.AutoField(primary_key=True)
-    item_status = models.CharField(max_length=20)
+    item_status = models.CharField(max_length=20, choices=item_statuses, default = 'active')
     item_name = models.CharField(max_length=200)
     img_link = models.CharField(max_length=200)
     short_description = models.CharField(max_length=255)
     long_description = models.CharField(max_length=1000)
     specification = models.CharField(max_length=400)
     item_cost = models.IntegerField()
-    item_type = models.CharField(max_length=10)
-    item_voltage = models.DecimalField(decimal_places=2, max_digits=10)
-    item_capacity = models.DecimalField(decimal_places=2, max_digits=10)
-    item_power = models.DecimalField(decimal_places=2, max_digits=10)
+    item_type = models.CharField(max_length=20, choices=item_types)
+    item_voltage = models.DecimalField(decimal_places=2, max_digits=10, default=None, blank=True, null=True)
+    item_capacity = models.DecimalField(decimal_places=2, max_digits=10, default=None, blank=True, null=True)
+    item_power = models.DecimalField(decimal_places=2, max_digits=10, default=None, blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'items'
 class plant_model(models.Model):
+    plant_statuses = {
+        'draft': 'draft',
+        'deleted': 'deleted',
+        'completed': 'completed',
+        'formed': 'formed',
+        'rejected': 'rejected',
+    }
     plant_id = models.AutoField(primary_key=True)
-    plant_status = models.CharField(max_length = 50)
-    creation_date = models.DateTimeField()
-    forming_date = models.DateTimeField()
-    finishing_date = models.DateTimeField()
+    plant_status = models.CharField(max_length=20, choices=plant_statuses, default = 'draft')
+    creation_date = models.DateTimeField(default = datetime.now)
+    forming_date = models.DateTimeField(default=None, blank=True, null=True)
+    finishing_date = models.DateTimeField(default=None, blank=True, null=True)
     creator_login = models.CharField(max_length=50)
-    moderator_login = models.CharField(max_length=50)
-    generation = models.DecimalField(decimal_places=2, max_digits=10)
-    saving = models.DecimalField(decimal_places=2, max_digits=10)
-    latitude = models.DecimalField(decimal_places=5, max_digits=8)
-    fio = models.CharField(max_length=255)
+    moderator_login = models.CharField(max_length=50, default=None, blank=True, null=True)
+    generation = models.DecimalField(decimal_places=2, max_digits=10, default=None, blank=True, null=True)
+    saving = models.DecimalField(decimal_places=2, max_digits=10, default=None, blank=True, null=True)
+    latitude = models.DecimalField(decimal_places=5, max_digits=8, default=None, blank=True, null=True)
+    fio = models.CharField(max_length=255, default=None, blank=True, null=True)
     class Meta:
         managed = False
         db_table = 'plants'
--- SolarEnergy/SolarEnergy/views.py
+++ SolarEnergy/SolarEnergy/views.py
@@ -17,12 +17,15 @@ def GetItem(request, id):
 
 def GetPlantRequest(request, id):
     plant_items = []
-    plant = plant_model.objects.get(creator_login = login, plant_status = 'draft')
-    temp_plant = plant_model.objects.filter(creator_login = login, plant_status = 'draft').values()
-    item2plant_list = item2plant_model.objects.filter(plant_id = plant.plant_id).values()
-    for item2plant in item2plant_list:
-        plant_items.append(item2plant)
-    data = {'data':{'items': AddLineChanges(item_model.objects.values()), 'plant_req':plant_items, 'plant':temp_plant}}
+    plant = {}
+    temp_plant = plant_model.objects.filter(plant_id = id, plant_status = 'draft').values()
+    if temp_plant:
+        item2plant_list = item2plant_model.objects.filter(plant_id = id).values()
+        for item2plant in item2plant_list:
+            plant_items.append(item2plant)
+        for item in temp_plant:
+            plant = item
+    data = {'data':{'items': AddLineChanges(item_model.objects.values()), 'plant_req':plant_items, 'plant':plant}}
     return render(request, 'plant_req_page.html', data)
 
 def GetPlantItems(request):
@@ -34,13 +37,9 @@ def GetPlantItems(request):
     data = {'data':{'items':AddLineChanges(sorted_list),'searchText':search_request}}
     return render(request, 'plant_items_page.html', data)
 
-def Add2Plant(request, login='andrew'):
+def Add2Plant(request):
     temp_item_id = request.POST['item_id']
-    temp_plant_id = 0
-    for plant in plant_model.objects.values():
-        if plant['creator_login'] == login and plant['plant_status'] == 'draft':
-            temp_plant_id = int(plant['plant_id'])
-            break
+    temp_plant_id = request.POST['plant_id']
     records = item2plant_model.objects.filter(item_id = temp_item_id, plant_id = temp_plant_id).values()
     if not records:
         max_id = item2plant_model.objects.aggregate(Max('relate_id'))
@@ -53,17 +52,19 @@ def Add2Plant(request, login='andrew'):
         item2plant.save()
     return GetPlantItems(request)
     
-def DelPlant(request, login, plant_id):
-    print("!")
-    #print[creator_login]
-    #conn = psycopg2.connect(dbname="solarenergy", host="127.0.0.1", user="student", password="root", port="5432")
-    #cursor = conn.cursor()
-    return GetPlantItems(request)
-    #cursor.execute('SELECT * FROM items')
-    #rows = cursor.fetchall()
-    #for table in rows:
-    #    print(table)
-    #conn.close()
+def DelPlant(request):
+    temp_plant_id = request.POST['plant_id']
 
-    #print(item_model.objects.all())
-    #print(temp_str)
+    print(1)
+
+    conn = psycopg2.connect(dbname="solarenergy", host="127.0.0.1", user="student", password="root", port="5432")
+
+    cursor = conn.cursor()
+    cursor.execute(str("UPDATE plants set plant_status = 'deleted' where plant_id = ") + str(temp_plant_id))
+    
+    conn.commit()   # реальное выполнение команд sql1
+    
+    cursor.close()
+    conn.close()
+
+    return GetPlantItems(request)
